2031cea082bf31c26ac1a191f3479fa3c513fa81
Daniel Tran <daniel.tran11@gmail.com>
2020-11-21 19:52:03 +1100

    Move chapter count mappings to the common module

--- src/meaningless/bible_yaml_downloader.py
+++ src/meaningless/bible_yaml_downloader.py
@@ -3,77 +3,6 @@ from meaningless import bible_list_extractor
 from meaningless.utilities import yaml_file_interface, common
 from ruamel.yaml import YAML
 
-# Storing the value of each book with its total number of chapters.
-# This assumes the default books and chapter counts in well-known Bible translations such as NIV and ESV.
-__chapter_count_mappings = {
-    'Genesis': 50,
-    'Exodus': 40,
-    'Leviticus': 27,
-    'Numbers': 36,
-    'Deuteronomy': 34,
-    'Joshua': 24,
-    'Judges': 21,
-    'Ruth': 4,
-    '1 Samuel': 31,
-    '2 Samuel': 24,
-    '1 Kings': 22,
-    '2 Kings': 25,
-    '1 Chronicles': 29,
-    '2 Chronicles': 36,
-    'Ezra': 10,
-    'Nehemiah': 13,
-    'Esther': 10,
-    'Job': 42,
-    'Psalm': 150,
-    'Proverbs': 31,
-    'Ecclesiastes': 12,
-    'Song Of Songs': 8,
-    'Isaiah': 66,
-    'Jeremiah': 52,
-    'Lamentations': 5,
-    'Ezekiel': 48,
-    'Daniel': 12,
-    'Hosea': 14,
-    'Joel': 3,
-    'Amos': 9,
-    'Obadiah': 1,
-    'Jonah': 4,
-    'Micah': 7,
-    'Nahum': 3,
-    'Habakkuk': 3,
-    'Zephaniah': 3,
-    'Haggai': 2,
-    'Zechariah': 14,
-    'Malachi': 4,
-    'Matthew': 28,
-    'Mark': 16,
-    'Luke': 24,
-    'John': 21,
-    'Acts': 28,
-    'Romans': 16,
-    '1 Corinthians': 16,
-    '2 Corinthians': 13,
-    'Galatians': 6,
-    'Ephesians': 6,
-    'Phillippians': 4,
-    'Colossians': 4,
-    '1 Thessalonians': 5,
-    '2 Thessalonians': 3,
-    '1 Timothy': 6,
-    '2 Timothy': 4,
-    'Titus': 3,
-    'Philemon': 1,
-    'Hebrews': 13,
-    'James': 5,
-    '1 Peter': 5,
-    '2 Peter': 3,
-    '1 John': 5,
-    '2 John': 1,
-    '3 John': 1,
-    'Jude': 1,
-    'Revelation': 22
-}
-
 # A list of translations that have known omitted passages.
 # Translations missing from this list are either not supported, or handle omitted passages in some way.
 __translations_with_omitted_passages = {
@@ -104,11 +33,12 @@ def yaml_download(book, file_location=os.getcwd(), show_passage_numbers=True, tr
     :return: 0 if the download was successful. Non-zero value if an error occurred.
     """
     # Check against passing invalid books
-    if book.title() not in __chapter_count_mappings.keys():
-        print('WARNING: "{0}" is not a valid book'.format(book))
+    if common.get_chapter_count(book, translation) <= 0:
+        print('WARNING: "{0}" is not a valid book, or "{1}" is an unsupported translation'.format(book, translation))
         return 1
-    return yaml_download_passage_range(book, 1, 1, __chapter_count_mappings[book], common.get_end_of_chapter(),
-                                       file_location, show_passage_numbers, translation, strip_whitespaces)
+    return yaml_download_passage_range(book, 1, 1, common.get_chapter_count(book, translation),
+                                       common.get_end_of_chapter(), file_location, show_passage_numbers,
+                                       translation, strip_whitespaces)
 
 
 def yaml_download_passage(book, chapter, passage, file_location=os.getcwd(), show_passage_numbers=True,
@@ -202,9 +132,9 @@ def yaml_download_passage_range(book, chapter_from, passage_from, chapter_to, pa
     """
     # Standardise letter casing with minimal impact to the resulting YAML file
     book = book.title()
-    # Check against passing invalid books
-    if book not in __chapter_count_mappings.keys():
-        print('WARNING: "{0}" is not a valid book'.format(book))
+
+    if common.get_chapter_count(book, translation) <= 0:
+        print('WARNING: "{0}" is not a valid book, or "{1}" is an unsupported translation'.format(book, translation))
         return 1
 
     is_translation_with_omitted_passages = translation in __translations_with_omitted_passages.keys()
--- src/meaningless/utilities/common.py
+++ src/meaningless/utilities/common.py
@@ -30,6 +30,98 @@ def get_end_of_chapter():
     return 9000
 
 
+def get_chapter_count(book, translation='NIV'):
+    """
+    A helper function to return the number of chapter in a given book for a particular translation.
+    :param book: Name of the book
+    :param translation: Translation code for the particular book. For example, 'NIV', 'ESV', 'NLT'
+    :return: Number of chapters in the book. 0 usually means an invalid book or unsupported translation.
+
+    >>> get_chapter_count('Ecclesiastes')
+    12
+    >>> get_chapter_count('Barnabas')
+    0
+    >>> get_chapter_count('Ecclesiastes', translation='msg')
+    0
+    """
+    # Standardise letter casing to help find the key easier
+    book = book.title()
+    # This is the default mapping of books to their chapter counts
+    chapter_count_mappings = {
+        'Genesis': 50,
+        'Exodus': 40,
+        'Leviticus': 27,
+        'Numbers': 36,
+        'Deuteronomy': 34,
+        'Joshua': 24,
+        'Judges': 21,
+        'Ruth': 4,
+        '1 Samuel': 31,
+        '2 Samuel': 24,
+        '1 Kings': 22,
+        '2 Kings': 25,
+        '1 Chronicles': 29,
+        '2 Chronicles': 36,
+        'Ezra': 10,
+        'Nehemiah': 13,
+        'Esther': 10,
+        'Job': 42,
+        'Psalm': 150,
+        'Proverbs': 31,
+        'Ecclesiastes': 12,
+        'Song Of Songs': 8,
+        'Isaiah': 66,
+        'Jeremiah': 52,
+        'Lamentations': 5,
+        'Ezekiel': 48,
+        'Daniel': 12,
+        'Hosea': 14,
+        'Joel': 3,
+        'Amos': 9,
+        'Obadiah': 1,
+        'Jonah': 4,
+        'Micah': 7,
+        'Nahum': 3,
+        'Habakkuk': 3,
+        'Zephaniah': 3,
+        'Haggai': 2,
+        'Zechariah': 14,
+        'Malachi': 4,
+        'Matthew': 28,
+        'Mark': 16,
+        'Luke': 24,
+        'John': 21,
+        'Acts': 28,
+        'Romans': 16,
+        '1 Corinthians': 16,
+        '2 Corinthians': 13,
+        'Galatians': 6,
+        'Ephesians': 6,
+        'Phillippians': 4,
+        'Colossians': 4,
+        '1 Thessalonians': 5,
+        '2 Thessalonians': 3,
+        '1 Timothy': 6,
+        '2 Timothy': 4,
+        'Titus': 3,
+        'Philemon': 1,
+        'Hebrews': 13,
+        'James': 5,
+        '1 Peter': 5,
+        '2 Peter': 3,
+        '1 John': 5,
+        '2 John': 1,
+        '3 John': 1,
+        'Jude': 1,
+        'Revelation': 22
+    }
+    # TODO Additional logic can be added here that changes chapter_count_mappings based on a given translation
+
+    if book not in chapter_count_mappings.keys() or is_unsupported_translation(translation):
+        return 0
+    return chapter_count_mappings[book]
+
+
 def get_page(url):
     """
     A helper function that returns the contents of a web page.
