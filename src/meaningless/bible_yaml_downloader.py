105783f056902e3bd574c96a9cd63914abd9c38e
Daniel Tran <daniel.tran11@gmail.com>
2020-11-30 20:45:34 +1100

    Better location flexibility when downloading YAML passages

--- src/meaningless/bible_yaml_downloader.py
+++ src/meaningless/bible_yaml_downloader.py
@@ -20,72 +20,88 @@ class YAMLDownloader:
                  'John 5:4', 'Acts 8:37', 'Acts 15:34', 'Acts 24:7', 'Acts 28:29', 'Romans 16:24']
     }
 
-    def __init__(self, translation='NIV', show_passage_numbers=True, file_location=os.getcwd(),
+    def __init__(self, translation='NIV', show_passage_numbers=True, default_directory=os.getcwd(),
                  strip_excess_whitespace=False, include_misc_info=False):
         """
         :param translation: Translation code for the particular passage. For example, 'NIV', 'ESV', 'NLT'
         :param show_passage_numbers: If True, any present passage numbers are preserved.
-        :param file_location: Directory containing the downloaded YAML file. Defaults to the current working directory.
+        :param default_directory: Directory containing the downloaded YAML file.
+                                  Defaults to the current working directory.
         :param strip_excess_whitespace: If True, passages don't retain leading & trailing whitespaces as well as
                                         newline characters.
         :param include_misc_info: If True, additional information is included in the YAML file, such as the translation.
         """
         self.translation = translation
         self.show_passage_numbers = show_passage_numbers
-        self.file_location = file_location
+        self.default_directory = default_directory
         self.strip_excess_whitespace = strip_excess_whitespace
         self.include_misc_info = include_misc_info
 
-    def download_passage(self, book, chapter, passage):
+    def download_passage(self, book, chapter, passage, file_path=''):
         """
         Downloads a single passage as a YAML file
         :param book: Name of the book
         :param chapter: Chapter number
         :param passage: Passage number
+        :param file_path: When specified, saves the YAML file to this location with a custom filename and extension.
+                          Using this parameter will take priority over the default_directory class property.
+                          Defaults to the default_directory path with the book as the file name, and ends in .yaml
         :return: 0 if the download was successful. Non-zero value if an error occurred.
         """
-        return self.download_passage_range(book, chapter, passage, chapter, passage)
+        return self.download_passage_range(book, chapter, passage, chapter, passage, file_path)
 
-    def download_passages(self, book, chapter, passage_from, passage_to):
+    def download_passages(self, book, chapter, passage_from, passage_to, file_path=''):
         """
         Downloads a range of passages of the same chapter as a YAML file
         :param book: Name of the book
         :param chapter: Chapter number
         :param passage_from: First passage number to get
         :param passage_to: Last passage number to get
+        :param file_path: When specified, saves the YAML file to this location with a custom filename and extension.
+                          Using this parameter will take priority over the default_directory class property.
+                          Defaults to the default_directory path with the book as the file name, and ends in .yaml
         :return: 0 if the download was successful. Non-zero value if an error occurred.
         """
-        return self.download_passage_range(book, chapter, passage_from, chapter, passage_to)
+        return self.download_passage_range(book, chapter, passage_from, chapter, passage_to, file_path)
 
-    def download_chapter(self, book, chapter):
+    def download_chapter(self, book, chapter, file_path=''):
         """
         Downloads a single chapter as a YAML file
         :param book: Name of the book
         :param chapter: Chapter number
+        :param file_path: When specified, saves the YAML file to this location with a custom filename and extension.
+                          Using this parameter will take priority over the default_directory class property.
+                          Defaults to the default_directory path with the book as the file name, and ends in .yaml
         :return: 0 if the download was successful. Non-zero value if an error occurred.
         """
-        return self.download_passage_range(book, chapter, 1, chapter, common.get_end_of_chapter())
+        return self.download_passage_range(book, chapter, 1, chapter, common.get_end_of_chapter(), file_path)
 
-    def download_chapters(self, book, chapter_from, chapter_to):
+    def download_chapters(self, book, chapter_from, chapter_to, file_path=''):
         """
         Downloads a range of passages from a specified chapter selection as a YAML file
         :param book: Name of the book
         :param chapter_from: First chapter number to get
         :param chapter_to: Last chapter number to get
+        :param file_path: When specified, saves the YAML file to this location with a custom filename and extension.
+                          Using this parameter will take priority over the default_directory class property.
+                          Defaults to the default_directory path with the book as the file name, and ends in .yaml
         :return: 0 if the download was successful. Non-zero value if an error occurred.
         """
-        return self.download_passage_range(book, chapter_from, 1, chapter_to, common.get_end_of_chapter())
+        return self.download_passage_range(book, chapter_from, 1, chapter_to, common.get_end_of_chapter(), file_path)
 
-    def download_book(self, book):
+    def download_book(self, book, file_path=''):
         """
         Downloads a specific book of the Bible and saves it as a YAML file
         :param book: Name of the book
+        :param file_path: When specified, saves the YAML file to this location with a custom filename and extension.
+                          Using this parameter will take priority over the default_directory class property.
+                          Defaults to the default_directory path with the book as the file name, and ends in .yaml
         :return: 0 if the download was successful. Non-zero value if an error occurred.
         """
         return self.download_passage_range(book, 1, 1, common.get_chapter_count(book, self.translation),
-                                           common.get_end_of_chapter())
+                                           common.get_end_of_chapter(), file_path)
 
-    def download_passage_range(self, book, chapter_from, passage_from, chapter_to, passage_to):
+    def download_passage_range(self, book, chapter_from, passage_from, chapter_to, passage_to, file_path=''):
         """
         Downloads a range of passages from one specific passage to another passage as a YAML file
         :param book: Name of the book
@@ -93,6 +109,10 @@ class YAMLDownloader:
         :param passage_from: First passage number to get in the first chapter
         :param chapter_to: Last chapter number to get
         :param passage_to: Last passage number to get in the last chapter
+        :param file_path: When specified, saves the YAML file to this location with a custom filename and extension.
+                          Using this parameter will take priority over the default_directory class property.
+                          Defaults to the default_directory path with the book as the file name, and ends in .yaml
+        :return: 0 if the download was successful. Non-zero value if an error occurred.
         """
         # Standardise letter casing with minimal impact to the resulting YAML file
         book = book.title()
@@ -160,8 +180,11 @@ class YAMLDownloader:
                 document[book][chapter][passage_num] = passage
                 passage_num += 1
 
-        return yaml_file_interface.write('{0}/{1}/{2}.yaml'.format(self.file_location, self.translation, book),
-                                         document)
+        if len(file_path) <= 0:
+            file_location = os.path.join(self.default_directory, '{0}.yaml'.format(book))
+        else:
+            file_location = file_path
+        return yaml_file_interface.write(file_location, document)
 
 if __name__ == "__main__":
     # Run this section when run as a standalone script. Don't run this part when being imported.
--- test/unit_tests_bible_yaml_downloader.py
+++ test/unit_tests_bible_yaml_downloader.py
@@ -13,99 +13,113 @@ class UnitTests(unittest.TestCase):
     #       All other methods will simply be interpreted as test helper functions.
 
     def test_yaml_download(self):
-        download_path = './tmp/test_yaml_download'
-        bible = YAMLDownloader(file_location=download_path)
+        download_path = './tmp/test_yaml_download/NIV'
+        bible = YAMLDownloader(default_directory=download_path)
         bible.download_book('Philemon')
-        self.assertTrue(filecmp.cmp('{0}/NIV/Philemon.yaml'.format(download_path),
+        self.assertTrue(filecmp.cmp('{0}/Philemon.yaml'.format(download_path),
                                     './static/NIV/test_yaml_download.yaml'),
                         'Files do not match')
 
     def test_yaml_download_without_passage_numbers(self):
-        download_path = './tmp/test_yaml_download_without_passage_numbers'
-        bible = YAMLDownloader(file_location=download_path, show_passage_numbers=False)
+        download_path = './tmp/test_yaml_download_without_passage_numbers/NIV'
+        bible = YAMLDownloader(default_directory=download_path, show_passage_numbers=False)
         bible.download_book('Philemon')
-        self.assertTrue(filecmp.cmp('{0}/NIV/Philemon.yaml'.format(download_path),
+        self.assertTrue(filecmp.cmp('{0}/Philemon.yaml'.format(download_path),
                                     './static/NIV/test_yaml_download_without_passage_numbers.yaml'),
                         'Files do not match')
 
     def test_yaml_download_invalid_book(self):
-        download_path = './tmp/test_yaml_download_invalid_book'
-        bible = YAMLDownloader(file_location=download_path)
+        download_path = './tmp/test_yaml_download_invalid_book/NIV'
+        bible = YAMLDownloader(default_directory=download_path)
         bible.download_book('Barnabas')
         # An invalid book should fail fast and not bother with downloading
         self.assertFalse(os.path.exists(download_path), 'File should not have downloaded')
 
     def test_yaml_download_nlt(self):
-        download_path = './tmp/test_yaml_download_nlt'
-        bible = YAMLDownloader(file_location=download_path, translation='NLT')
+        download_path = './tmp/test_yaml_download_nlt/NLT'
+        bible = YAMLDownloader(default_directory=download_path, translation='NLT')
         bible.download_book('Philemon')
-        self.assertTrue(filecmp.cmp('{0}/NLT/Philemon.yaml'.format(download_path),
+        self.assertTrue(filecmp.cmp('{0}/Philemon.yaml'.format(download_path),
                                     './static/NLT/test_yaml_download_nlt.yaml'),
                         'Files do not match')
 
     def test_yaml_download_omitted_passage(self):
-        download_path = './tmp/test_yaml_download_omitted_passage'
-        bible = YAMLDownloader(file_location=download_path, translation='NLT')
+        download_path = './tmp/test_yaml_download_omitted_passage/NLT'
+        bible = YAMLDownloader(default_directory=download_path, translation='NLT')
         bible.download_book('Romans')
-        text = yaml_file_interface.read('{0}/NLT/Romans.yaml'.format(download_path))['Romans'][16][24]
+        text = yaml_file_interface.read('{0}/Romans.yaml'.format(download_path))['Romans'][16][24]
         self.assertEqual('', text, 'Files do not match')
 
     def test_yaml_download_with_stripped_whitespaces(self):
-        download_path = './tmp/test_yaml_download_with_stripped_whitespaces'
-        bible = YAMLDownloader(file_location=download_path, strip_excess_whitespace=True)
+        download_path = './tmp/test_yaml_download_with_stripped_whitespaces/NIV'
+        bible = YAMLDownloader(default_directory=download_path, strip_excess_whitespace=True)
         bible.download_book('Philemon')
-        self.assertTrue(filecmp.cmp('{0}/NIV/Philemon.yaml'.format(download_path),
+        self.assertTrue(filecmp.cmp('{0}/Philemon.yaml'.format(download_path),
                                     './static/NIV/test_yaml_download_with_stripped_whitespaces.yaml'),
                         'Files do not match')
 
     def test_yaml_download_passage(self):
-        download_path = './tmp/test_yaml_download_passage'
-        bible = YAMLDownloader(file_location=download_path)
+        download_path = './tmp/test_yaml_download_passage/NIV'
+        bible = YAMLDownloader(default_directory=download_path)
         bible.download_passage('Philemon', 1, 1)
-        self.assertTrue(filecmp.cmp('{0}/NIV/Philemon.yaml'.format(download_path),
+        self.assertTrue(filecmp.cmp('{0}/Philemon.yaml'.format(download_path),
                                     './static/NIV/test_yaml_download_passage.yaml'),
                         'Files do not match')
 
     def test_yaml_download_passages(self):
-        download_path = './tmp/test_yaml_download_passages'
-        bible = YAMLDownloader(file_location=download_path)
+        download_path = './tmp/test_yaml_download_passages/NIV'
+        bible = YAMLDownloader(default_directory=download_path)
         bible.download_passages('Philemon', 1, 1, 3)
-        self.assertTrue(filecmp.cmp('{0}/NIV/Philemon.yaml'.format(download_path),
+        self.assertTrue(filecmp.cmp('{0}/Philemon.yaml'.format(download_path),
                                     './static/NIV/test_yaml_download_passages.yaml'),
                         'Files do not match')
 
     def test_yaml_download_chapter(self):
-        download_path = './tmp/test_yaml_download_chapter'
-        bible = YAMLDownloader(file_location=download_path)
+        download_path = './tmp/test_yaml_download_chapter/NIV'
+        bible = YAMLDownloader(default_directory=download_path)
         bible.download_chapter('1 John', 1)
-        self.assertTrue(filecmp.cmp('{0}/NIV/1 John.yaml'.format(download_path),
+        self.assertTrue(filecmp.cmp('{0}/1 John.yaml'.format(download_path),
                                     './static/NIV/test_yaml_download_chapter.yaml'),
                         'Files do not match')
 
     def test_yaml_download_chapters(self):
-        download_path = './tmp/test_yaml_download_chapter'
-        bible = YAMLDownloader(file_location=download_path)
+        download_path = './tmp/test_yaml_download_chapter/NIV'
+        bible = YAMLDownloader(default_directory=download_path)
         bible.download_chapters('1 John', 1, 3)
-        self.assertTrue(filecmp.cmp('{0}/NIV/1 John.yaml'.format(download_path),
+        self.assertTrue(filecmp.cmp('{0}/1 John.yaml'.format(download_path),
                                     './static/NIV/test_yaml_download_chapters.yaml'),
                         'Files do not match')
 
     def test_yaml_download_passage_range(self):
-        download_path = './tmp/test_yaml_download_passage_range'
-        bible = YAMLDownloader(file_location=download_path)
+        download_path = './tmp/test_yaml_download_passage_range/NIV'
+        bible = YAMLDownloader(default_directory=download_path)
         bible.download_passage_range('1 John', 1, 3, 1, 5)
-        self.assertTrue(filecmp.cmp('{0}/NIV/1 John.yaml'.format(download_path),
+        self.assertTrue(filecmp.cmp('{0}/1 John.yaml'.format(download_path),
                                     './static/NIV/test_yaml_download_passage_range.yaml'),
                         'Files do not match')
 
     def test_yaml_download_with_misc_info(self):
-        download_path = './tmp/test_yaml_download_with_misc_info'
-        bible = YAMLDownloader(file_location=download_path, include_misc_info=True)
+        download_path = './tmp/test_yaml_download_with_misc_info/NIV'
+        bible = YAMLDownloader(default_directory=download_path, include_misc_info=True)
         bible.download_book('Philemon')
-        document = yaml_file_interface.read('{0}/NIV/Philemon.yaml'.format(download_path))
+        document = yaml_file_interface.read('{0}/Philemon.yaml'.format(download_path))
         # For some reason, the misc. info is always placed after the passage contents in these tests?
         self.assertEqual('English', document['Info']['Language'], 'Language info is not correct')
         self.assertEqual('NIV', document['Info']['Translation'], 'Translation info is not correct')
+
+    def test_yaml_download_with_different_file_path(self):
+        download_path = './tmp/test_yaml_download_with_different_file_path/NIV'
+        custom_path = './tmp/test_yaml_download_with_different_file_path.txt'
+        bible = YAMLDownloader(default_directory=download_path)
+        # File contents should remain the same with and without the custom path parameter
+        bible.download_passage_range('1 John', 1, 3, 1, 5)
+        bible.download_passage_range('1 John', 1, 3, 1, 5, file_path=custom_path)
+        self.assertTrue(filecmp.cmp('{0}/1 John.yaml'.format(download_path), custom_path), 'Files do not match')
+        # A default directory with a relative reference should also preserve the same file contents
+        bible.default_directory = './tmp/../tmp/test_yaml_download_with_different_file_path/Fardel'
+        bible.download_passage_range('1 John', 1, 3, 1, 5)
+        self.assertTrue(filecmp.cmp('{0}/1 John.yaml'.format(bible.default_directory), custom_path),
+                        'Files do not match')
 
 if __name__ == "__main__":
     unittest.main()
