4c0171b64a8075994781f545e732b8ee1eb94825
Daniel Tran <daniel.tran11@gmail.com>
2020-12-04 19:11:48 +1100

    Add custom error messages with custom exceptions

--- src/meaningless/utilities/exceptions.py
+++ src/meaningless/utilities/exceptions.py
@@ -11,6 +11,7 @@ class UnsupportedTranslationError(BaseError):
         An exception thrown when handling translations that are not currently supported
         :param translation: Translation code. For example, 'NIV', 'ESV', 'NLT'
         """
+        super(UnsupportedTranslationError, self).__init__('{0} is an unsupported translation'.format(translation))
         self.translation = translation
 
 
@@ -25,6 +26,15 @@ class InvalidPassageError(BaseError):
         :param passage_to: Last passage number to get in the last chapter
         :param translation: Translation code. For example, 'NIV', 'ESV', 'NLT'
         """
+        if chapter_from == chapter_to:
+            if passage_from == passage_to:
+                passage = '{0}:{1}'.format(chapter_from, passage_from)
+            else:
+                passage = '{0}:{1} - {2}'.format(chapter_from, passage_from, passage_to)
+        else:
+            passage = '{0}:{1} - {2}:{3}'.format(chapter_from, passage_from, chapter_to, passage_to)
+        super(InvalidPassageError, self).__init__('{0} {1} is an invalid passage in the {2} translation'.format(
+            book, passage, translation))
         self.book = book
         self.chapter_from = chapter_from
         self.passage_from = passage_from
@@ -40,5 +50,7 @@ class InvalidSearchError(BaseError):
         :param search:  Search string used on the Bible Gateway site
         :param translation: Translation code. For example, 'NIV', 'ESV', 'NLT'
         """
+        super(InvalidSearchError, self).__init__('{0} returned no search results using the {1} translation'.format(
+            search, translation))
         self.search_string = search
         self.translation = translation
