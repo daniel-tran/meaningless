aa4da6e8b435368f66e2651fcfc3994323fef3de
Daniel Tran <daniel.tran11@gmail.com>
2021-01-01 08:39:49 +1100

    Add __init__ file for utilities module
    Also to better control the generated documentation for this module in Sphinx

---
+++ meaningless/utilities/__init__.py
@@ -0,0 +1,4 @@
+# This file is used as a way of defining accessible modules when importing the utilities library.
+# Note that common is not included in the default module import, since it's mostly intended for internal use.
+
+from meaningless.utilities import yaml_file_interface, exceptions
