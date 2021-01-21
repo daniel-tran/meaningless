7f3a84456886c7be33b2e02f7b61c04fbec162f4
Daniel Tran <daniel.tran11@gmail.com>
2021-01-21 19:03:56 +1100

    Transition YAML Extractor and Downloader to using the base class implementations
    Also removes existing code duplication, since the base class was largely taken from the YAML components anyway

--- test/unit_tests_bible_yaml_downloader.py
+++ test/unit_tests_bible_yaml_downloader.py
@@ -1,9 +1,9 @@
 import unittest
 import sys
-import filecmp
-from timeit import default_timer
 sys.path.append('../')
-from meaningless import YAMLDownloader, yaml_file_interface, InvalidSearchError, InvalidPassageError
+from meaningless import YAMLDownloader, yaml_file_interface
+
+# These tests just test for certain components which differ from the base downloader
 
 
 class UnitTests(unittest.TestCase):
@@ -11,213 +11,13 @@ class UnitTests(unittest.TestCase):
     # Note: Tests will only be run if they are prefixed with test_ in their method name.
     #       All other methods will simply be interpreted as test helper functions.
 
-    @staticmethod
-    def get_test_translation():
-        """
-        A helper function to define the standardised translation for this set of tests.
-        :return: Translation code
-        :rtype: str
-        """
-        return 'WEB'
-
-    @staticmethod
-    def get_test_directory(translation='WEB'):
-        """
-        A helper function to determine the working directory for this set of unit tests
-        :param translation: Translation code for the tests. For example, 'NIV', 'ESV', 'NLT'
-        :type translation: str
-        :return: Directory path containing readable files
-        :rtype: str
-        """
-        return './static/unit_tests_bible_yaml_downloader/{0}'.format(translation)
-
-    def test_yaml_download(self):
-        download_path = './tmp/test_yaml_download/'
-        bible = YAMLDownloader(default_directory=download_path, translation=self.get_test_translation())
-        bible.download_book('Philemon')
-        downloaded_yaml = yaml_file_interface.read('{0}/Philemon.yaml'.format(download_path))
-        static_file_path = '{0}/test_yaml_download.yaml'.format(self.get_test_directory())
-        static_yaml = yaml_file_interface.read(static_file_path)
-        self.assertEqual(downloaded_yaml['Philemon'], static_yaml['Philemon'], 'Passage contents do not match')
-
-    def test_yaml_download_without_passage_numbers(self):
-        download_path = './tmp/test_yaml_download_without_passage_numbers/'
-        bible = YAMLDownloader(default_directory=download_path, show_passage_numbers=False,
-                               translation=self.get_test_translation())
-        bible.download_book('Philemon')
-        downloaded_yaml = yaml_file_interface.read('{0}/Philemon.yaml'.format(download_path))
-        static_file_path = '{0}/test_yaml_download_without_passage_numbers.yaml'.format(self.get_test_directory())
-        static_yaml = yaml_file_interface.read(static_file_path)
-        self.assertEqual(downloaded_yaml['Philemon'], static_yaml['Philemon'], 'Passage contents do not match')
-
-    def test_yaml_download_invalid_book(self):
-        download_path = './tmp/test_yaml_download_invalid_book/'
-        bible = YAMLDownloader(default_directory=download_path)
-        # An invalid book should fail fast and not bother with downloading
-        self.assertRaises(InvalidPassageError, bible.download_book, 'Barnabas')
-
-    def test_yaml_download_kjv(self):
-        download_path = './tmp/test_yaml_download_kjv/'
-        bible = YAMLDownloader(default_directory=download_path, translation='kjv')
-        bible.download_book('Philemon')
-        downloaded_yaml = yaml_file_interface.read('{0}/Philemon.yaml'.format(download_path))
-        static_file_path = '{0}/test_yaml_download_kjv.yaml'.format(self.get_test_directory('KJV'))
-        static_yaml = yaml_file_interface.read(static_file_path)
-        self.assertEqual(downloaded_yaml['Philemon'], static_yaml['Philemon'], 'Passage contents do not match')
-
-    def test_yaml_download_omitted_passage(self):
-        download_path = './tmp/test_yaml_download_omitted_passage/'
-        bible = YAMLDownloader(default_directory=download_path, translation='NLT')
-        bible.download_book('Romans')
-        text = yaml_file_interface.read('{0}/Romans.yaml'.format(download_path))['Romans'][16][24]
-        self.assertEqual('', text, 'Files do not match')
-
-    def test_yaml_download_with_stripped_whitespaces(self):
-        download_path = './tmp/test_yaml_download_with_stripped_whitespaces/'
-        bible = YAMLDownloader(default_directory=download_path, strip_excess_whitespace=True,
-                               translation=self.get_test_translation())
-        bible.download_book('Philemon')
-        downloaded_yaml = yaml_file_interface.read('{0}/Philemon.yaml'.format(download_path))
-        static_file_path = '{0}/test_yaml_download_with_stripped_whitespaces.yaml'.format(self.get_test_directory())
-        static_yaml = yaml_file_interface.read(static_file_path)
-        self.assertEqual(downloaded_yaml['Philemon'], static_yaml['Philemon'], 'Passage contents do not match')
-
-    def test_yaml_download_passage(self):
-        download_path = './tmp/test_yaml_download_passage/'
-        bible = YAMLDownloader(default_directory=download_path, translation=self.get_test_translation())
-        bible.download_passage('Philemon', 1, 1)
-        downloaded_yaml = yaml_file_interface.read('{0}/Philemon.yaml'.format(download_path))
-        static_file_path = '{0}/test_yaml_download_passage.yaml'.format(self.get_test_directory())
-        static_yaml = yaml_file_interface.read(static_file_path)
-        self.assertEqual(downloaded_yaml['Philemon'], static_yaml['Philemon'], 'Passage contents do not match')
-
-    def test_yaml_download_passages(self):
-        download_path = './tmp/test_yaml_download_passages/'
-        bible = YAMLDownloader(default_directory=download_path, translation=self.get_test_translation())
-        bible.download_passages('Philemon', 1, 1, 3)
-        downloaded_yaml = yaml_file_interface.read('{0}/Philemon.yaml'.format(download_path))
-        static_file_path = '{0}/test_yaml_download_passages.yaml'.format(self.get_test_directory())
-        static_yaml = yaml_file_interface.read(static_file_path)
-        self.assertEqual(downloaded_yaml['Philemon'], static_yaml['Philemon'], 'Passage contents do not match')
-
-    def test_yaml_download_chapter(self):
-        download_path = './tmp/test_yaml_download_chapter/'
-        bible = YAMLDownloader(default_directory=download_path, translation=self.get_test_translation())
-        bible.download_chapter('1 John', 1)
-        downloaded_yaml = yaml_file_interface.read('{0}/1 John.yaml'.format(download_path))
-        static_file_path = '{0}/test_yaml_download_chapter.yaml'.format(self.get_test_directory())
-        static_yaml = yaml_file_interface.read(static_file_path)
-        self.assertEqual(downloaded_yaml['1 John'], static_yaml['1 John'], 'Passage contents do not match')
-
-    def test_yaml_download_chapters(self):
-        download_path = './tmp/test_yaml_download_chapters/'
-        bible = YAMLDownloader(default_directory=download_path, translation=self.get_test_translation())
-        bible.download_chapters('1 John', 1, 3)
-        downloaded_yaml = yaml_file_interface.read('{0}/1 John.yaml'.format(download_path))
-        static_file_path = '{0}/test_yaml_download_chapters.yaml'.format(self.get_test_directory())
-        static_yaml = yaml_file_interface.read(static_file_path)
-        self.assertEqual(downloaded_yaml['1 John'], static_yaml['1 John'], 'Passage contents do not match')
-
-    def test_yaml_download_passage_range(self):
-        download_path = './tmp/test_yaml_download_passage_range/'
-        bible = YAMLDownloader(default_directory=download_path, translation=self.get_test_translation())
-        bible.download_passage_range('1 John', 1, 3, 1, 5)
-        downloaded_yaml = yaml_file_interface.read('{0}/1 John.yaml'.format(download_path))
-        static_file_path = '{0}/test_yaml_download_passage_range.yaml'.format(self.get_test_directory())
-        static_yaml = yaml_file_interface.read(static_file_path)
-        self.assertEqual(downloaded_yaml['1 John'], static_yaml['1 John'], 'Passage contents do not match')
-
-    def test_yaml_download_with_misc_info(self):
-        download_path = './tmp/test_yaml_download_with_misc_info/NIV'
-        bible = YAMLDownloader(default_directory=download_path)
-        bible.download_passage('Philemon', 1, 1)
-        document = yaml_file_interface.read('{0}/Philemon.yaml'.format(download_path))
-        # For some reason, the misc. info is always placed after the passage contents in these tests?
-        self.assertEqual('English', document['Info']['Language'], 'Language info is not correct')
-        self.assertEqual('NIV', document['Info']['Translation'], 'Translation info is not correct')
-
-    def test_yaml_download_with_different_file_path(self):
-        download_path = './tmp/test_yaml_download_with_different_file_path/NIV'
-        custom_path = './tmp/test_yaml_download_with_different_file_path.txt'
-        bible = YAMLDownloader(default_directory=download_path)
-        # File contents should remain the same with and without the custom path parameter
-        bible.download_passage_range('1 John', 1, 3, 1, 5)
-        bible.download_passage_range('1 John', 1, 3, 1, 5, file_path=custom_path)
-        self.assertTrue(filecmp.cmp('{0}/1 John.yaml'.format(download_path), custom_path), 'Files do not match')
-        # A default directory with a relative reference should also preserve the same file contents
-        bible.default_directory = './tmp/../tmp/test_yaml_download_with_different_file_path/Fardel'
-        bible.download_passage_range('1 John', 1, 3, 1, 5)
-        self.assertTrue(filecmp.cmp('{0}/1 John.yaml'.format(bible.default_directory), custom_path),
-                        'Files do not match')
-
-    def test_yaml_download_with_excessive_values(self):
-        download_path = './tmp/test_yaml_download_with_excessive_values/'
-        static_path = '{0}/test_yaml_download_with_excessive_values'.format(self.get_test_directory())
-        branching_paths = [
-            '{0}/Chapter-1'.format(download_path),
-            '{0}/Chapter1000'.format(download_path),
-            '{0}/Chapter5'.format(download_path),
-        ]
-        bible = YAMLDownloader(default_directory=branching_paths[0], translation=self.get_test_translation())
-        # This should obtain the first passage of the first chapter
-        bible.download_passage_range('1 John', -1, -1, -1, -1)
-        downloaded_yaml = yaml_file_interface.read('{0}/1 John.yaml'.format(bible.default_directory))
-        static_yaml = yaml_file_interface.read('{0}/Chapter-1/1 John.yaml'.format(static_path))
-        self.assertEqual(downloaded_yaml['1 John'], static_yaml['1 John'], 'Passage contents do not match')
-        # This does NOT get the last passage of the last chapter, but is instead deemed invalid.
-        # This is consistent with how the Bible Gateway site handles when the starting passage is too high.
-        bible.default_directory = branching_paths[1]
-        self.assertRaises(InvalidSearchError, bible.download_passage_range, '1 John', 1000, 1000, 1000, 1000)
-        # Gets the last chapter of the book.
-        # This is valid because the starting passage is a correct number from the chapter.
-        bible.default_directory = branching_paths[2]
-        bible.download_passage_range('1 John', 1000, 1, 1000, 1000)
-        downloaded_yaml = yaml_file_interface.read('{0}/1 John.yaml'.format(bible.default_directory))
-        static_yaml = yaml_file_interface.read('{0}/Chapter5/1 John.yaml'.format(static_path))
-        self.assertEqual(downloaded_yaml['1 John'], static_yaml['1 John'], 'Passage contents do not match')
-
-    def test_yaml_download_lowercase_translation(self):
-        download_path = './tmp/test_yaml_download_lowercase_translation/NLT'
-        bible = YAMLDownloader(default_directory=download_path, translation='NLT')
-        bible.download_book('Philemon')
-        bible.translation = 'nlt'
-        lowercase_file_path = '{0}/Philemon-lowercase.yaml'.format(download_path)
-        bible.download_book('Philemon', file_path=lowercase_file_path)
-        self.assertTrue(filecmp.cmp('{0}/Philemon.yaml'.format(download_path), lowercase_file_path),
-                        'Files do not match')
-
-    def test_yaml_download_single_process_benchmark(self):
-        download_path = './tmp/test_yaml_download_single_process_benchmark'
-        multi_processed_yaml = '{0}/multi_processed.yaml'.format(download_path)
-        single_processed_yaml = '{0}/single_processed.yaml'.format(download_path)
-        bible = YAMLDownloader(default_directory=download_path, enable_multiprocessing=True)
-
-        start = default_timer()
-        bible.download_book('Acts', file_path=multi_processed_yaml)
-        end = default_timer()
-        multi_processed_time = end - start
-
-        bible.enable_multiprocessing = False
-        start = default_timer()
-        bible.download_book('Acts', file_path=single_processed_yaml)
-        end = default_timer()
-        single_processed_time = end - start
-        print('Multi-processed download took {0} seconds\n'
-              'Single-processed download took {1} seconds'.format(multi_processed_time, single_processed_time))
-
-        self.assertTrue(filecmp.cmp(multi_processed_yaml, single_processed_yaml), 'Files do not match')
-        self.assertTrue(multi_processed_time <= single_processed_time,
-                        'Multi-processed download should have been faster')
-
-    def test_yaml_download_with_ascii_punctuation(self):
-        download_path = './tmp/test_yaml_download_with_ascii_punctuation'
-        static_file_path = '{0}/test_yaml_download_with_ascii_punctuation.yaml'.format(self.get_test_directory())
-        bible = YAMLDownloader(default_directory=download_path, translation=self.get_test_translation(),
-                               use_ascii_punctuation=True)
-        bible.download_book('Ecclesiastes')
-        downloaded_yaml = yaml_file_interface.read('{0}/Ecclesiastes.yaml'.format(download_path))
-        static_yaml = yaml_file_interface.read(static_file_path)
-        self.assertEqual(downloaded_yaml['Ecclesiastes'], static_yaml['Ecclesiastes'], 'Passage contents do not match')
+    def test_yaml_downloader_settings(self):
+        bible = YAMLDownloader()
+        self.assertEqual(bible.file_extension, '.yaml', 'Extension is incorrect')
+        self.assertEqual(bible.file_writing_function.__module__, yaml_file_interface.write.__module__,
+                         'Module of writing function is incorrect')
+        self.assertEqual(bible.file_writing_function.__name__, yaml_file_interface.write.__name__,
+                         'Name of writing function is incorrect')
 
 if __name__ == "__main__":
     unittest.main()
