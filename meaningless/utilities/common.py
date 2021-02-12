e64c6699aebce651b2a1dcc670971536cbeb3ffc
Daniel Tran <daniel.tran11@gmail.com>
2021-02-13 10:55:03 +1100

    Add BRG translation support

--- meaningless/utilities/common.py
+++ meaningless/utilities/common.py
@@ -20,7 +20,7 @@ def is_unsupported_translation(translation):
     """
     # These translations are particularly difficult to extract information from due to them using
     # non-conventional page layouts compared to other translations: 'MOUNCE', 'VOICE', 'MSG', 'PHILLIPS'
-    return translation.upper() not in ['ASV', 'AKJV', 'ESV', 'KJV', 'KJ21', 'LEB', 'MEV', 'NASB',
+    return translation.upper() not in ['ASV', 'AKJV', 'BRG', 'ESV', 'KJV', 'KJ21', 'LEB', 'MEV', 'NASB',
                                        'NET', 'NIV', 'NKJV', 'NLT', 'NLV', 'NRSV', 'WEB', 'YLT']
 
 
--- test/unit_tests_bible_translations.py
+++ test/unit_tests_bible_translations.py
@@ -205,5 +205,10 @@ class UnitTests(unittest.TestCase):
         self.check_baseline_passages(translation)
         self.check_omitted_passages(translation)
 
+    def test_translation_brg(self):
+        translation = 'BRG'
+        self.check_baseline_passages(translation)
+        self.check_omitted_passages(translation)
+
 if __name__ == "__main__":
     unittest.main()
