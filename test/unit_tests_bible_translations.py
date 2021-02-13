24c0044362b7fb144c70aa2564b67295ec37b363
Daniel Tran <daniel.tran11@gmail.com>
2021-02-13 11:16:04 +1100

    Add ESVUK translation support

--- test/unit_tests_bible_translations.py
+++ test/unit_tests_bible_translations.py
@@ -215,5 +215,10 @@ class UnitTests(unittest.TestCase):
         self.check_baseline_passages(translation)
         self.check_omitted_passages(translation)
 
+    def test_translation_esvuk(self):
+        translation = 'ESVUK'
+        self.check_baseline_passages(translation)
+        self.check_omitted_passages(translation)
+
 if __name__ == "__main__":
     unittest.main()
