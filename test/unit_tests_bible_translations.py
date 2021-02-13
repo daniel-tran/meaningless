30f45a7b90d808ffd697254a2565c862a255bb32
Daniel Tran <daniel.tran11@gmail.com>
2021-02-13 11:06:59 +1100

    Add EHV translation support

--- test/unit_tests_bible_translations.py
+++ test/unit_tests_bible_translations.py
@@ -210,5 +210,10 @@ class UnitTests(unittest.TestCase):
         self.check_baseline_passages(translation)
         self.check_omitted_passages(translation)
 
+    def test_translation_ehv(self):
+        translation = 'EHV'
+        self.check_baseline_passages(translation)
+        self.check_omitted_passages(translation)
+
 if __name__ == "__main__":
     unittest.main()
