51e2824ac23d1abc4a456490570a80f9e26b9aec
Daniel Tran <daniel.tran11@gmail.com>
2022-06-04 15:11:05 +1000

    Add experimental scripts
    To showcase potential functionality that might make it into the library in the future, although that is not a guarantee. Also includes helper scripts that can help with debugging and automation.

---
+++ experimental/download_books.py
@@ -0,0 +1,45 @@
+import multiprocessing
+import os
+import sys
+sys.path.append('../')
+from meaningless import YAMLDownloader
+
+
+def download_book_with_debug_messages(yaml_downloader, book_name):
+    """
+    Wrapper function for download_book with some extra print messages
+
+    :param yaml_downloader: YAML Downloader object
+    :type yaml_downloader: YAMLDownloader
+    :param book_name: Name of the book
+    :type book_name: str
+    """
+    print('Downloading {0}...'.format(book_name))
+    yaml_downloader.download_book(book_name)
+    print('Completed download for {0}.'.format(book_name))
+
+
+if __name__ == "__main__":
+    # Run this section when run as a standalone script. Don't run this part when being imported.
+    translation = input('Enter a translation code (this will be converted to UPPERCASE): ').upper()
+    output_folder = input('Enter the full directory name where files should be written to: ')
+    downloader = YAMLDownloader(translation=translation, default_directory=os.path.join(output_folder, translation),
+                                enable_multiprocessing=False)
+    books = ['Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy',
+             'Joshua', 'Judges', 'Ruth', '1 Samuel', '2 Samuel',
+             '1 Kings', '2 Kings', '1 Chronicles', '2 Chronicles', 'Ezra',
+             'Nehemiah', 'Esther', 'Job', 'Psalm', 'Proverbs',
+             'Ecclesiastes', 'Song of Songs', 'Isaiah', 'Jeremiah', 'Lamentations',
+             'Ezekiel', 'Daniel', 'Hosea', 'Joel', 'Amos',
+             'Obadiah', 'Jonah', 'Micah', 'Nahum', 'Habakkuk',
+             'Zephaniah', 'Haggai', 'Zechariah', 'Malachi', 'Matthew',
+             'Mark', 'Luke', 'John', 'Acts', 'Romans',
+             '1 Corinthians', '2 Corinthians', 'Galatians', 'Ephesians', 'Philippians',
+             'Colossians', '1 Thessalonians', '2 Thessalonians', '1 Timothy', '2 Timothy',
+             'Titus', 'Philemon', 'Hebrews', 'James', '1 Peter',
+             '2 Peter', '1 John', '2 John', '3 John', 'Jude', 'Revelation']
+    params = [(downloader, book) for book in books]
+
+    process_pool = multiprocessing.Pool(6)
+    process_pool.starmap(download_book_with_debug_messages, params, 6)
+    print('Completed download of the following books: {0}'.format(str(books)))
---
+++ experimental/find_keyword_online.py
@@ -0,0 +1,71 @@
+from bs4 import BeautifulSoup
+from urllib.parse import urlencode
+import sys
+sys.path.append('../')
+from meaningless.utilities import common
+
+
+def find_keyword(keyword, translation, search_type):
+    """
+    Gets a list of passage matches using the Bible Gateway advanced search. This only returns the top 500 search
+    results and will always search across all books.
+
+    :param keyword: Search text
+    :type keyword: str
+    :param translation: Translation code. For example, 'NIV', 'ESV', 'NLT'
+    :type translation: str
+    :param search_type: Search mode. Should be set to one of the following strings:
+                        'any' (match any word),
+                        'all' (match all words),
+                        'phrase' (match all words in given order)
+    :type search_type: str
+    :return: 2D array of data, where the first list contains the passage names of each match and the second
+             list contains the passage contents for each item in the first list
+    :rtype: list
+    """
+    source_site_params = urlencode({'version': translation, 'search': keyword, 'searchtype': search_type,
+                                    'resultspp': '500'})
+    soup = BeautifulSoup(common.get_page(
+        'https://www.biblegateway.com/quicksearch/?{0}'.format(source_site_params)), 'html.parser')
+
+    # Headers are for any paragraph headings present with the passage text.
+    # Elements with the 'bible-item-extras' class are neighbouring hyperlinks that have the same parent element
+    # as the passage text.
+    removable_tags = soup.find_all('h3') + soup.find_all('div', {'class': 'bible-item-extras'})
+    [tag.decompose() for tag in removable_tags]
+
+    passage_titles = [tag.text for tag in soup.find_all('a', {'class': 'bible-item-title'})]
+    passage_texts = [tag.text.strip() for tag in soup.find_all('div', {'class': 'bible-item-text'})]
+
+    return [passage_titles, passage_texts]
+
+
+if __name__ == "__main__":
+    # Shows some results from Ecclesiastes
+    # For some reason, 'Book of Isaiah' shows up as a match?
+    print(str(find_keyword('everything is meaningless', 'NIV', 'all')))
+
+    input('Next: Using phrase as search type')
+    print(str(find_keyword('everything is meaningless', 'NIV', 'phrase')))
+
+    input('Next: Using any as search type')
+    print(str(find_keyword('everything is meaningless', 'NIV', 'any')))
+
+    input('Next: Using invalid search type (defaults to type all)')
+    print(str(find_keyword('meaningless', 'NIV', 'freed')))
+
+    input('Next: No search results')
+    print(str(find_keyword('Glove', 'NIV', 'any')))
+
+    input('Next: Using HTML tag as search criteria')
+    # Normally, this would redirect back to the homepage
+    print(str(find_keyword('</span>', 'NIV', 'any')))
+
+    input('Next: Using an invalid translation')
+    print(str(find_keyword('meaningless', 'FREED', 'any')))
+
+    input('Next: Empty search text')
+    print(str(find_keyword('', 'NIV', 'any')))
+
+    input('Next: Bugged input that shows HTML tags in the passage text')
+    print(str(find_keyword('Lord.', 'NIV', 'any')))
